683d5c243a48becd4ef70329208fd3ed3322dc3c
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-09-26 15:10:59 +0900

    Main Push

--- my-app/src/Manager/Manager.js
+++ my-app/src/Manager/Manager.js
@@ -1,36 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Manager.module.css";
-import Web3 from "web3";
 
 class Manager extends Component {
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      window.alert(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3;
-
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
-
-    this.setState({ account: accounts[0] });
-    // Smart Contract Code를 여기다가 연결시키면 된다!
-  }
-
   render() {
     return (
       <div className={styles.button}>
