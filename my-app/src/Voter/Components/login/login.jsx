e2bfdf702b778a8613df9ff1547375bc665d8c1b
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-09-27 17:13:22 +0900

    login components

--- my-app/src/Voter/Components/login/login.jsx
+++ my-app/src/Voter/Components/login/login.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import styles from "./login.module.css";
+
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 
-const Login = () => (
-  <div className={styles.login}>
-    <h1> Name </h1>
-    <h1> Address </h1>
-    <Button variant="contained" endIcon={<SendIcon />}>
-      Confirm
-    </Button>
-  </div>
-);
-export default Login;
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function Login() {
+  return (
+    <>
+      <h1> Welcome to the Voting dApp</h1>
+      <div className={styles.login}>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField className={styles.name} label="Name" variant="outlined" />
+          <TextField
+            className={styles.address}
+            label="Address"
+            variant="outlined"
+          />
+        </Box>
+        <Button
+          className={styles.confirm}
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          Confirm
+        </Button>
+      </div>
+    </>
+  );
+}
