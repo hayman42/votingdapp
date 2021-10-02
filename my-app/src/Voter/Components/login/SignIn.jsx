8e034985ea293f80973361fe460c5ad161c43278
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-02 20:21:28 +0900

    End

--- my-app/src/Voter/Components/login/SignIn.jsx
+++ my-app/src/Voter/Components/login/SignIn.jsx
@@ -10,6 +10,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useHistory } from "react-router";
 
 function Copyright(props) {
   return (
@@ -32,12 +33,15 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
+  const history = useHistory();
+
   const handleSubmit = event => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
-      email: data.get("email"),
+      name: data.get("name"),
+      address: data.get("address"),
       password: data.get("password"),
     });
   };
@@ -102,6 +106,9 @@ export default function SignIn() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={() => {
+                history.push("/poll_list");
+              }}
             >
               Sign In
             </Button>
