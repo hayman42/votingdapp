a90f979ef26737c31bdeaa63b3fa033367b53947
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-03 15:38:46 +0900

    correct

--- my-app/src/Voter/Components/poll_list/poll_list.jsx
+++ my-app/src/Voter/Components/poll_list/poll_list.jsx
@@ -2,7 +2,6 @@ import React, { Fragment, useEffect, useState } from "react";
 
 import MenuBar from "../commonComponents/menuBar/menuBar";
 import Header from "../commonComponents/header/header";
-// import Question from "../question/question";
 
 import styles from "./poll_list.module.css";
 import { Container } from "@mui/material";
@@ -11,13 +10,6 @@ import Button from "@mui/material/Button";
 import { useHistory } from "react-router";
 
 import axios from "axios";
-
-// const data = [
-//   "지지하고 있는 정당은?",
-//   "20대이신가요?",
-//   "대학생이신가요?",
-//   "좋아하는 과일은?",
-// ];
 
 export default function Poll_list() {
   const history = useHistory();
@@ -29,6 +21,7 @@ export default function Poll_list() {
     async function fetch() {
       const res = await axios.get("http://localhost:5000/poll/list");
       setData(res.data);
+      console.log(res.data);
     }
     fetch();
   }, []);
--- my-app/src/Voter/Mypage.js
+++ my-app/src/Voter/Mypage.js
@@ -1,62 +1,65 @@
-import { Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
+import {
+  Button,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 
-export default function Mypage() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        async function fetch() {
-            const res = await axios.get("http://localhost:5000/user/info", {
-                params: {
-                    name: "asdf"
-                }
-            });
-            const { voteinfo } = res.data;
-            setData(voteinfo);
-        }
-        fetch();
-    }, []);
+import { useHistory } from "react-router";
 
-    return (
-        <Container>
-            <h3>My page</h3>
-            <Fragment>
-                <TableContainer component={Paper}>
-                    <Table  >
-                        <TableHead>
-                            <TableRow>
-                                {
-                                    ["#", "날짜", "조사", "선택한 후보", "승/패"].map((x, i) =>
-                                        <TableCell align="left" key={i}><b>{x}</b></TableCell>)
-                                }
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {
-                                data.map((x, i) =>
-                                    <TableRow key={i}>
-                                        <TableCell align="left">
-                                            {i + 1}
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            {x.createdAt}
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            {x.content}
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            {x.candidate}
-                                        </TableCell>
-                                        <TableCell align="left">
-                                            {x.win}
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Fragment>
-        </Container >
-    );
-};
+export default function Mypage() {
+  const history = useHistory();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetch() {
+      const res = await axios.get("http://localhost:5000/user/info", {
+        params: {
+          name: "asdf",
+        },
+      });
+      const { voteinfo } = res.data;
+      setData(voteinfo);
+    }
+    fetch();
+  }, []);
+
+  return (
+    <Container>
+      <h3>My page</h3>
+      <Fragment>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {["#", "날짜", "조사", "선택한 후보", "승/패"].map((x, i) => (
+                  <TableCell align="left" key={i}>
+                    <b>{x}</b>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((x, i) => (
+                <TableRow key={i}>
+                  <TableCell align="left">{i + 1}</TableCell>
+                  <TableCell align="left">{x.createdAt}</TableCell>
+                  <TableCell align="left">{x.content}</TableCell>
+                  <TableCell align="left">{x.candidate}</TableCell>
+                  <TableCell align="left">{x.win}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Fragment>
+    </Container>
+  );
+}
